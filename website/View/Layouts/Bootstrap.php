36e90b0305770afa1cdabe9921ee7ad0d996b248
rampmaster <rampmaster@gmail.com>
2014-04-05 22:48:32 -0300

    save: mejora en la distribución del layout

--- website/View/Layouts/Bootstrap.php
+++ website/View/Layouts/Bootstrap.php
@@ -1,241 +1,145 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <!-- Design by http://delaf.cl -->
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es">
-    <head>
-        <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
-        <meta name="generator" content="SowerPHP"/>
-        <title><?=$_header_title?></title>
-        <link rel="shortcut icon" href="<?=$_base?>/img/favicon.png" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-        <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
-		<link rel="stylesheet" type="text/css" href="<?=$_base?>/assets/css/style.css" />
-        
-        <!--[if lt IE 9]>
-          <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
-        <![endif]-->
-        <link rel="shortcut icon" href="<?=$_base?>/bootstrap/img/favicon.ico">
-        <link rel="apple-touch-icon" href="<?=$_base?>/bootstrap/img/apple-touch-icon.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="<?=$_base?>/bootstrap/img/apple-touch-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="<?=$_base?>/bootstrap/img/apple-touch-icon-114x114.png">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <meta name="generator" content="SowerPHP"/>
+    <title><?= $_header_title ?></title>
+    <link rel="shortcut icon" href="<?= $_base ?>/img/favicon.png"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="<?= $_base ?>/assets/css/style.css"/>
 
-        <!-- CSS code from Bootply.com editor -->
-        
-        <style type="text/css">
+    <!--[if lt IE 9]>
+    <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <link rel="shortcut icon" href="<?= $_base ?>/bootstrap/img/favicon.ico">
+    <link rel="apple-touch-icon" href="<?= $_base ?>/bootstrap/img/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?= $_base ?>/bootstrap/img/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?= $_base ?>/bootstrap/img/apple-touch-icon-114x114.png">
 
-        </style>		
-    </head>
-    <body>
+    <!-- CSS code from Bootply.com editor -->
+
+    <style type="text/css">
+
+    </style>
+</head>
+<body>
 <header class="navbar navbar-default navbar-fixed-top" role="banner">
-  <div class="container">
-    <div class="navbar-header">
-      <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a href="/" class="navbar-brand"><?=$_body_title?></a>
-    </div>
-    <nav class="collapse navbar-collapse" role="navigation">
-      <ul class="nav navbar-nav">
-		<?php
-		$links = array();
-		foreach($_nav_website as $link => &$name) {
-			if($link[0]=='/') $link = $_base.$link;
-			if(is_array($name)) echo '<li><a href="'.$link.'" title="'.$name['title'].'">'.$name['name'].'</a></li>';
-			else echo '<li><a href="'.$link.'">'.$name.'</a></li>';
-		}
-		?>
-      </ul>
-    </nav>
-  </div>
-</header>
-<div id="masthead">  
-  <div class="container">
-      <div class="row">
-        <div class="col-md-7">
-          <h1>Bootstrap Sidebar
-            <p class="lead">With Affix and Scrollspy</p>
-          </h1>
+    <div class="container">
+        <div class="navbar-header">
+            <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a href="/" class="navbar-brand"><?= $_body_title ?></a>
         </div>
-        <div class="col-md-5">
-            <div class="well well-lg"> 
-              <div class="row">
-                <div class="col-sm-6">
-        	      	<img src="//placehold.it/180x100" class="img-responsive">
+        <nav class="collapse navbar-collapse" role="navigation">
+            <ul class="nav navbar-nav">
+                <?php
+                $links = array();
+                foreach ($_nav_website as $link => &$name) {
+                    if ($link[0] == '/') $link = $_base . $link;
+                    if (is_array($name)) echo '<li><a href="' . $link . '" title="' . $name['title'] . '">' . $name['name'] . '</a></li>';
+                    else echo '<li><a href="' . $link . '">' . $name . '</a></li>';
+                }
+                ?>
+            </ul>
+        </nav>
+    </div>
+</header>
+<div id="masthead">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7">
+                <h1>Bienvenidos a GNU-Linux Chile
+                    <p class="lead">Tu portal de información sobre GNU y Linux</p>
+                </h1>
+            </div>
+            <div class="col-md-5">
+                <div class="well well-lg">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <img src="//placehold.it/180x100" class="img-responsive">
+                        </div>
+                        <div class="col-sm-6">
+                            Some text here
+                        </div>
+                    </div>
                 </div>
-                <div class="col-sm-6">
-	              	Some text here
-                </div>
-              </div>
             </div>
         </div>
-      </div> 
-  </div><!-- /cont -->
+    </div>
+    <!-- /cont -->
 </div>
 <!-- Begin Body -->
 <div class="container">
-	<div class="row">
-  			<div class="col-md-3" id="leftCol">
-              
-              	<ul class="nav nav-stacked" id="sidebar">
-                  <li><a href="#sec0">Section 0</a></li>
-                  <li><a href="#sec1">Section 1</a></li>
-                  <li><a href="#sec2">Section 2</a></li>
-                  <li><a href="#sec3">Section 3</a></li>
-                  <li><a href="#sec4">Section 4</a></li>
-              	</ul>				
-              
-      		</div>  
-      		<div class="col-md-9">
-				<!-- CONTENIDO PRINCIPAL -->
-				<?php
-				$message = \sowerphp\core\Model_Datasource_Session::message();
-				if($message) echo '<div class="session_message">',$message,'</div>';
-				echo $_content;
-				?>
-				<!-- FIN DEL CONTENIDO PRINCIPAL -->
-              	<h2 id="sec0">Content</h2>
-                At Bootply we like to build simple Bootstrap templates that utilize the code Bootstap CSS without a lot of customization. Sure you can 
-              	find a lot of Bootstrap themes and inspiration, but these templates tend to be heavy on customization.
-              	
-            
-           
-              	<hr class="col-md-12">
-              	Rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
-                dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores 
-                eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
-                sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
-                Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut.              
-              	Rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
-                dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores 
-                eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
-                sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
-                Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut!
-              	
-              
-              
-              	<h2 id="sec1">Content</h2>
-              	<p>
-                Rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
-                dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut.
-              	</p>
-              	<div class="row">
-                  <div class="col-md-6">
-                    <div class="panel panel-default">
-                      <div class="panel-heading"><h3>Hello.</h3></div>
-                      <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
-                      Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
-                      dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
-                      Aliquam in felis sit amet augue.
-                      </div>
-                    </div>
-                  </div>
-                  <div class="col-md-6">
-                      <div class="panel panel-default">
-                      <div class="panel-heading"><h3>Hello.</h3></div>
-                      <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
-                      Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
-                      dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
-                      Aliquam in felis sit amet augue.
-                      </div>
-                    </div>
-                  </div>  
-              	</div>
-              
-              	<hr>
-              
-              	<h2 id="sec2">Section 2</h2>
-              	<p>
-                Rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
-                dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores 
-                eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
-                sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
-                Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut!
-              	</p>
-              	<div class="row">
-              		<div class="col-md-4"><img src="//placehold.it/300x300" class="img-responsive"></div>
-                  	<div class="col-md-4"><img src="//placehold.it/300x300" class="img-responsive"></div>
-                  	<div class="col-md-4"><img src="//placehold.it/300x300" class="img-responsive"></div>
-              	</div>
-              
-              	<hr>
-              
-              	<h2 id="sec3">Section 3</h2>
-      			Images are responsive sed @mdo but sum are more fun peratis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
-                totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
-                dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores 
-                eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
-                sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
-                Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut..
-              	<br>
-                Fos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
-                sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
-                Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut..
-              
-              
-              	<h2 id="sec4">Section 4</h2>
-      			Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
-                totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
-                dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores 
-                eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
-                sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
-                Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
-              
-              
-              	<hr>
-              <h4><small><?=(is_array($_footer)?implode(' ', $_footer):$_footer),"\n"?></small></h4>
-              	<hr>
-              	
-              	
-      		</div> 
-  	</div>
+    <div class="row">
+        <!-- CONTENIDO PRINCIPAL -->
+        <?php
+        $message = \sowerphp\core\Model_Datasource_Session::message();
+        if ($message) echo '<div class="session_message">', $message, '</div>';
+        echo $_content;
+        ?>
+        <!-- FIN DEL CONTENIDO PRINCIPAL -->
+    </div>
+    <div class="row">
+        <div class="col-md-9 col-md-offset-3">
+            <hr>
+            <h4>
+                <small><?= (is_array($_footer) ? implode(' ', $_footer) : $_footer), "\n" ?></small>
+            </h4>
+            <hr>
+        </div>
+    </div>
 </div>
-        
-        <script type='text/javascript' src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+<script type='text/javascript' src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 
 
-        <script type='text/javascript' src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+<script type='text/javascript' src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
 
-		        <!-- JavaScript jQuery code from Bootply.com editor  -->
-        
-        <script type='text/javascript'>
-        
-        $(document).ready(function() {
-        
-            $('#sidebar').affix({
-      offset: {
-        top: 235
-      }
-});
+<!-- JavaScript jQuery code from Bootply.com editor  -->
 
-var $body   = $(document.body);
-var navHeight = $('.navbar').outerHeight(true) + 10;
+<script type='text/javascript'>
 
-$body.scrollspy({
-	target: '#leftCol',
-	offset: navHeight
-});
+    $(document).ready(function () {
 
-/* smooth scrolling sections */
-$('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top - 50
-        }, 1000);
-        return false;
-      }
-    }
-});
-        
+        $('#sidebar').affix({
+            offset: {
+                top: 235
+            }
         });
-        
-        </script>
-		
 
-    </body>
+        var $body = $(document.body);
+        var navHeight = $('.navbar').outerHeight(true) + 10;
+
+        $body.scrollspy({
+            target: '#leftCol',
+            offset: navHeight
+        });
+
+        /* smooth scrolling sections */
+        $('a[href*=#]:not([href=#])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top - 50
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+
+    });
+
+</script>
+
+
+</body>
 </html>
 
